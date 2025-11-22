b8c5824a5f2cfe2161e1a448700c2ee38ac7c346
Lakh Bawa <lakhveer.bawa@viamo.io>
2025-11-22 17:27:20 -0500

    fix: bug

--- routes/web.php
+++ routes/web.php
@@ -3,9 +3,7 @@
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
@@ -18,3 +16,5 @@ Route::middleware('auth')->group(function () {
 });
 
 require __DIR__.'/auth.php';
+
+require __DIR__.'/tenant.php';
