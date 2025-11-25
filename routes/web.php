d7ee7078aac2356d74b7cb06fb3a8d98510bf606
Lakh Bawa <lakhveer.bawa@viamo.io>
2025-11-25 07:58:56 -0500

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -4,7 +4,10 @@ use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 
-
+Route::get('test', function() {
+    echo 'test';
+    return 'test successful';
+});
 Route::get('/dashboard', function () {
     return view('dashboard');
 })->middleware(['auth', 'verified'])->name('dashboard');
